7a7d98a6e706d3c08b362a391318edb2f99d4fde
Jdoreporrett <j.dore-porrett@griffith.edu.au>
2021-08-19 11:34:48 +1000

    Python Folder

---
+++ WorkshopContent.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug 19 11:06:08 2021
+
+@author: s2989594
+"""
+
+pressures = [0.273, 0.275, 0.277, 0.276]
+print ('First item of pressure:', pressures[0])
+
